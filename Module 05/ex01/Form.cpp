150660a9c7ac909f482470f7e7ecabe9cbc093d4
Bernardo <bbento-e@student.42porto.com>
2024-10-10 16:50:01 +0100

    continuing ex01

--- Module 05/ex01/Form.cpp
+++ Module 05/ex01/Form.cpp
@@ -1,5 +1,10 @@
-//
-// Created by kali on 10/10/24.
-//
-
 #include "Form.hpp"
+
+Form::Form() : name("Default"), sign(false), sigGrade(1), excGrade(1){
+	std::cout << "Form default constructor called" << std::endl;
+}
+
+Form::Form(std::string nm, const int sGrade, const int xGrade) : name(nm), sigGrade(sGrade), excGrade(xGrade) {
+	std::cout << "Form constructor called" << std::endl;
+	if ()
+}
--- Module 05/ex01/Form.hpp
+++ Module 05/ex01/Form.hpp
@@ -1,6 +1,32 @@
 #ifndef FORM_HPP
 #define FORM_HPP
 
+#include <iostream>
+#include "Bureaucrat.hpp"
 
+class Form {
+private:
+	const std::string name;
+	bool sign;
+	const int sigGrade;
+	const int excGrade;
+
+public:
+	Form();
+	Form(std::string nm, const int sGrade, const int xGrade);
+	Form(const Form& frm);
+	Form &operator=(const Form& frm);
+	~Form();
+
+	std::string getName();
+	bool isSigned();
+	int getSigGrade();
+	int getExcGrade();
+
+	using GradeTooHighException = Bureaucrat::GradeTooHighException;
+	using GradeTooLowException = Bureaucrat::GradeTooLowException;
+
+}
+std::ostream& operator<<(std::ostream& os, const Form& obj);
 
 #endif //FORM_HPP
