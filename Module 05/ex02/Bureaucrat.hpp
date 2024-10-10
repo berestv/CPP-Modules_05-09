a2dc5fd0315bf65bcd092505ca4182fe402bad54
Bernardo <bbento-e@student.42porto.com>
2024-10-10 19:49:32 +0100

    started ex02

---
+++ Module 05/ex02/Bureaucrat.hpp
@@ -0,0 +1,42 @@
+#ifndef BUREAUCRAT_HPP
+#define BUREAUCRAT_HPP
+
+#include <iostream>
+#include "Form.hpp"
+
+class Form;
+
+class Bureaucrat {
+private:
+	const std::string name;
+	int grade;
+
+	void setGrade(int grd);
+public:
+	Bureaucrat();
+	Bureaucrat(std::string nm, int grd);
+	Bureaucrat(const Bureaucrat& brcrt);
+	Bureaucrat &operator=(const Bureaucrat& brcrt);
+	~Bureaucrat();
+
+	std::string getName() const;
+	int getGrade() const;
+	static void signForm(Bureaucrat& bureau, Form& frm);
+
+	Bureaucrat operator++();
+	Bureaucrat operator--();
+
+	class GradeTooHighException : public std::exception {
+	public:
+		virtual const char * what() const throw();
+	};
+
+class GradeTooLowException : public std::exception {
+	public:
+		virtual const char * what() const throw();
+	};
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& obj);
+
+#endif // BUREAUCRAT_HPP
