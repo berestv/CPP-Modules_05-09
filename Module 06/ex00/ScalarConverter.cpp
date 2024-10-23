181e1d9d5fd5373cddb00ba749327a8306619ab6
berestv <bbento-e@student.42porto.com>
2024-10-23 15:36:10 +0100

    working on type conversion

--- Module 06/ex00/ScalarConverter.cpp
+++ Module 06/ex00/ScalarConverter.cpp
@@ -1,7 +1,29 @@
 #include "ScalarConverter.hpp"
+#include <cstdlib>
+
+ScalarConverter::ScalarConverter() {
+
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter &sc) {
+
+}
+
+ScalarConverter &ScalarConverter::operator=(const ScalarConverter &sc) {
+
+}
+
+ScalarConverter::~ScalarConverter() {
+
+}
 
 void ScalarConverter::convert(std::string stRep) {
-	double dbl = atof(stRep.c_str());
-	int intg = dbl;
-	float flt = std::stof(stRep);
+
+
+	double dbl = strtod(stRep.c_str(), NULL);
+	float flt = strtof(stRep.c_str(), NULL);
+}
+
+const char *ScalarConverter::InvalidInputExc::what() const throw() {
+	return "Invalid input! You need to enter a number or a pseudo literal.";
 }
--- Module 06/ex00/ScalarConverter.hpp
+++ Module 06/ex00/ScalarConverter.hpp
@@ -4,8 +4,19 @@
 #include <iostream>
 
 class ScalarConverter {
+private:
+	ScalarConverter();
+	ScalarConverter(const ScalarConverter& sc);
+	ScalarConverter &operator=(const ScalarConverter& sc);
+	~ScalarConverter();
+
 public:
 	virtual void convert(std::string stRep);
+
+	class InvalidInputExc : std::exception {
+	public:
+		virtual const char * what() const throw();
+	};
 };
 
 #endif //SCALARCONVERTER_HPP
