feb4e5ad6bde6d258b3d4cc0aad6daee7ebbfc2a
berestv <bbento-e@student.42porto.com>
2024-10-24 16:12:27 +0100

    created Base class

--- Module 06/ex02/Base.hpp
+++ Module 06/ex02/Base.hpp
@@ -1,4 +1,17 @@
 #ifndef BASE_HPP
 #define BASE_HPP
 
+#include <iostream>
+// typeinfo
+// HA! Gotcha.
+
+class Base {
+public:
+	virtual ~Base();
+
+	Base * generate(void);
+	void identify(Base* p);
+	void identify(Base& p);
+};
+
 #endif //BASE_HPP
