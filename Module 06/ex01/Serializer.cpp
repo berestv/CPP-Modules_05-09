c6e3cf1b26502916ce1e4651d19d9ee3011a15a0
berestv <bbento-e@student.42porto.com>
2024-10-24 15:27:00 +0100

    creating everything

--- Module 06/ex01/Serializer.cpp
+++ Module 06/ex01/Serializer.cpp
@@ -1,1 +1,16 @@
 #include "Serializer.hpp"
+
+Serialzier::Serialzier() {}
+Serialzier::Serialzier(const Serialzier &ser) { (void)ser; }
+Serialzier &Serialzier::operator=(const Serialzier &ser) { (void)ser; return *this; }
+Serialzier::~Serialzier() {}
+
+// FUNCTIONS
+
+uintptr_t Serialzier::serialize(Data *ptr) {
+
+}
+
+Data *Serialzier::deserialize(uintptr_t raw) {
+
+}
--- Module 06/ex01/Serializer.hpp
+++ Module 06/ex01/Serializer.hpp
@@ -1,4 +1,25 @@
 #ifndef SERIALIZER_HPP
 #define SERIALIZER_HPP
 
+#include <iostream>
+#include <stdint.h>
+
+struct Data {
+	int id;
+	std::string name;
+};
+
+class Serialzier {
+private:
+	Serialzier();
+	Serialzier(const Serialzier& ser);
+	Serialzier &operator=(const Serialzier& ser);
+	~Serialzier();
+
+public:
+	uintptr_t serialize(Data* ptr);
+	Data* deserialize(uintptr_t raw);
+
+};
+
 #endif //SERIALIZER_HPP
