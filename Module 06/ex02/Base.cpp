18e709191ea11565e0a4f2c965d7b8223d8c76c5
berestv <bbento-e@student.42porto.com>
2024-10-25 16:40:59 +0100

    Fixed memory leaks

--- Module 06/ex02/Base.cpp
+++ Module 06/ex02/Base.cpp
@@ -34,27 +34,28 @@ void Base::identify(Base *p) {
 		std::cout << "C" << std::endl;
 	else
 		std::cout << "Doesn't match A, B or C" << std::endl;
+	delete p;
 }
 
 void Base::identify(Base &p) {
 	try {
 		(void)dynamic_cast<A&>(p);
 		std::cout << "A" << std::endl;
-		return ;
+		return (void)delete &p;
 	}
 	catch (std::exception& e) {	}
 
 	try {
 		(void)dynamic_cast<B&>(p);
 		std::cout << "B" << std::endl;
-		return ;
+		return (void)delete &p;
 	}
 	catch (std::exception& e) { }
 
 	try {
 		(void)dynamic_cast<C&>(p);
 		std::cout << "C" << std::endl;
-		return ;
+		return (void)delete &p;
 	}
 	catch (std::exception& e) {	}
 }
