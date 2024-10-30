ed6f9673b887729b32cdd39e0b4cbe692cf92ced
berestv <bbento-e@student.42porto.com>
2024-10-30 18:45:21 +0000

    implemented max, min and swap

--- Module 07/ex00/whatever.hpp
+++ Module 07/ex00/whatever.hpp
@@ -3,10 +3,16 @@
 
 #include <iostream>
 
-template<typename T>
-
-T max (T& x, T& y) {
+template<typename T> T max (T& x, T& y) {
 	return (x >= y ? x : y);
+}
+
+template<typename T> T min (T& x, T& y) {
+	return (x <= y ? x : y);
+}
+
+template<typename T> T swap (T& x, T& y) {
+
 }
 
 #endif //WHATEVER_HPP
