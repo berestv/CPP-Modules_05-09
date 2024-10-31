9f5bea43df141d30d2775ea4912040688fba4979
berestv <bbento-e@student.42porto.com>
2024-10-31 16:13:22 +0000

    completed ex00

--- Module 07/ex00/whatever.hpp
+++ Module 07/ex00/whatever.hpp
@@ -3,16 +3,21 @@
 
 #include <iostream>
 
-template<typename T> T max (T& x, T& y) {
+template<typename T>
+T max (T& x, T& y) {
 	return (x >= y ? x : y);
 }
 
-template<typename T> T min (T& x, T& y) {
+template<typename T>
+T min (T& x, T& y) {
 	return (x <= y ? x : y);
 }
 
-template<typename T> T swap (T& x, T& y) {
-
+template<typename T>
+void swap (T& x, T& y) {
+	T swp = x;
+	x = y;
+	y = swp;
 }
 
 #endif //WHATEVER_HPP
