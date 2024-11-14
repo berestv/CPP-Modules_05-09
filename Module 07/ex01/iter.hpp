efd465addb77ab1dd9afd00cd2027fa1aff40961
berestv <bbento-e@student.42porto.com>
2024-11-14 18:04:43 +0000

    starting ex01

--- Module 07/ex01/iter.hpp
+++ Module 07/ex01/iter.hpp
@@ -2,7 +2,7 @@
 #define ITER_HPP
 
 template<typename T>
-void iter(T& arr, int len, T& func(T lmt)) {
+void iter(T& arr, int len, void (*func)(T&)) {
 	for (int i = 0; i <= len; ++i)
 		func(arr[i]);
 }
