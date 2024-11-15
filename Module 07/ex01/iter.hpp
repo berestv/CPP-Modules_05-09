63fbcaf1fe9abd6b1cde41e4b0e4274228e2e1cd
berestv <bbento-e@student.42porto.com>
2024-11-15 16:18:45 +0000

    starting ex01

--- Module 07/ex01/iter.hpp
+++ Module 07/ex01/iter.hpp
@@ -5,8 +5,9 @@
 
 template<typename T>
 void iter(T& arr, int len, void (*print)(T&)) {
-	for (int i = 0; i <= len; ++i)
+	for (int i = 0; i < len; ++i) {
 		print(arr[i]);
+	}
 }
 
 template<typename T>
