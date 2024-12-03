8155a43f06c31a73dc0c2406c5aacf1dfd6aed17
berestv <bbento-e@student.42porto.com>
2024-12-03 17:58:00 +0000

    added more tests

--- Module 07/ex02/main.cpp
+++ Module 07/ex02/main.cpp
@@ -17,5 +17,36 @@ int main() {
 		std::cerr << e.what() << std::endl;
 	}
 
+	try {
+		Array<int> arr(10);
+		std::cout << "O.O.B. test:" << std::endl;
+		for (unsigned int i = 0; i < arr.size(); i++)
+		{
+			std::cout << arr[i] << std::endl;
+			arr[i] = i;
+		}
+		arr[11] = 1;
+	}
+	catch (std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+
+	try {
+		Array<int> arr(10);
+		std::cout << "Array 1 values:" << std::endl;
+		for (unsigned int i = 0; i < arr.size(); i++)
+		{
+			std::cout << arr[i] << std::endl;
+			arr[i] = i;
+		}
+		Array<int> arr2 = arr;
+		arr2[3] = 100;
+		std::cout << "Array 2 values after [3] = 100:" << std::endl;
+		for (unsigned int i = 0; i < arr2.size(); i++)
+			std::cout << arr2[i] << std::endl;
+	}
+	catch (std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
 	return 0;
 }
