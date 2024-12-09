9c970d7bfbb9911a3a28ee202766cbde9d7534fa
berestv <bbento-e@student.42porto.com>
2024-12-09 16:48:46 +0000

    fixed, everything works

--- Module 08/ex00/easyfind.tpp
+++ Module 08/ex00/easyfind.tpp
@@ -6,14 +6,14 @@
 
 template<typename T>
 void easyFind(T& cont, int n){
-	T pos = std::find(cont.begin(), cont.end(), n);
+	typename T::iterator pos = std::find(cont.begin(), cont.end(), n);
 	if (pos == cont.end())
 		throw easyFind::NotFoundException();
-	std::cout << "Found " << n << " at " << pos << std::endl;
+	std::cout << "Found " << n << " at " << std::distance(cont.begin(), pos) << std::endl;
 }
 
 const char *easyFind::NotFoundException::what() const throw() {
-	return "";
+	return "Could not find value.";
 }
 
 #endif //EASYFIND_TPP
--- Module 08/ex00/main.cpp
+++ Module 08/ex00/main.cpp
@@ -2,8 +2,35 @@
 
 int main() {
 	try {
-		std::vector<int> container = { 21, 42, 35, 1, 29, 55, 3 };
+		std::cout << "Vector Test:" << std::endl;
+		std::vector<int> container;
+		container.push_back(21);
+		container.push_back(42);
+		container.push_back(35);
+		container.push_back(1);
+		container.push_back(29);
+		container.push_back(55);
+		container.push_back(3);
 		easyFind(container, 42);
+		easyFind(container, 43);
+	}
+	catch (std::exception &e) {
+		std::cerr << e.what() << std::endl;
+	}
+	std::cout << std::endl;
+	try {
+		std::cout << "List Test:" << std::endl;
+		std::list<int> container;
+		container.push_back(33);
+		container.push_back(526);
+		container.push_back(99);
+		container.push_back(7);
+		container.push_back(123);
+		container.push_back(82);
+		container.push_back(54);
+		easyFind(container, 7);
+		easyFind(container, 526);
+		easyFind(container, 500);
 	}
 	catch (std::exception &e) {
 		std::cerr << e.what() << std::endl;
