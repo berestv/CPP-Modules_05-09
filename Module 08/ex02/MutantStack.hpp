90b3598cbd19d4f5153442fbb52e706df1c78047
berestv <bbento-e@student.42porto.com>
2024-12-11 15:34:44 +0000

    building MutantStack

--- Module 08/ex02/MutantStack.hpp
+++ Module 08/ex02/MutantStack.hpp
@@ -1,8 +1,19 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-class MutantStack {
+#include <iostream>
+#include <stack>
 
+template<class T> class MutantStack : public std::stack<T> {
+public:
+	MutantStack();
+	MutantStack(const MutantStack& cpy);
+	MutantStack &operator=(const MutantStack& cpy);
+	~MutantStack();
+	typename std::stack<T>::container_type::iterator begin();
+	typename std::stack<T>::container_type::iterator end();
 };
+
+#include "MutantStack.tpp"
 
 #endif //MUTANTSTACK_HPP
