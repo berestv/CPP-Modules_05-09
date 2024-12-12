426a85e3a28a3129b93e29338f9fee3869a708f0
berestv <bbento-e@student.42porto.com>
2024-12-12 14:40:21 +0000

    testing features from stack in lists

--- Module 08/ex02/main.cpp
+++ Module 08/ex02/main.cpp
@@ -1,6 +1,7 @@
 #include "MutantStack.hpp"
 
 int main() {
+	std::cout << "Operations with stack:" << std::endl;
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
@@ -21,5 +22,28 @@ int main() {
 		++it;
 	}
 	std::stack<int> s(mstack);
+
+	std::cout << "Same operations with list:" << std::endl;
+	std::list<int> mlist;
+	mlist.push_back(5);
+	mlist.push_back(17);
+	std::cout << *mlist.begin() << std::endl;
+	mlist.pop_front();
+	std::cout << mlist.size() << std::endl;
+	mlist.push_back(3);
+	mlist.push_back(5);
+	mlist.push_back(737);
+
+	mlist.push_back(0);
+	std::list<int>::iterator mit = mlist.begin();
+	std::list<int>::iterator mite = mlist.end();
+	++mit;
+	--mite;
+	while (mit != mite) {
+		std::cout << *mit << std::endl;
+		++mit;
+	}
+	std::list<int> l(mlist);
+
 	return 0;
 }
