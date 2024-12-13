c7890aeb6303e95a53f29162324e2e81bd361727
berestv <bbento-e@student.42porto.com>
2024-12-13 14:22:50 +0000

    added more tests

--- Module 07/ex01/main.cpp
+++ Module 07/ex01/main.cpp
@@ -3,6 +3,11 @@
 int main() {
 	std::string arr[] = { "abc", "def", "ghi", "jkl" };
 	size_t len = sizeof(arr) / sizeof(arr[0]);
-
 	iter(arr, len, print);
+	std::cout << std::endl;
+
+	int arr2[] = { 0, 1, 2, 3 };
+	size_t l = 4;
+	iter(arr2, l, print);
+	std::cout << std::endl;
 }
