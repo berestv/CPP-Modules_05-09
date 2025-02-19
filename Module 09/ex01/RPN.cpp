f3290c83462cd9d07385e239690fe06f0396ce78
Bernardo <bbento-e@student.42porto.com>
2025-02-19 16:04:30 +0000

    Added valid character verification

--- Module 09/ex01/RPN.cpp
+++ Module 09/ex01/RPN.cpp
@@ -1,9 +1,6 @@
 #include "RPN.hpp"
 
 RPN::RPN(std::string operation) {
-	/*if (operation.find('.') != std::string::npos || operation.find(',') != std::string::npos
-	    || operation.find('(') != std::string::npos || operation.find(')') != std::string::npos)
-		throw ErrorException();*/
 	if (operation.find_first_not_of(" 0123456789+-*/") != std::string::npos)
 		throw ErrorException();
 
